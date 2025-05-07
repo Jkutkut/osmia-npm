3157506a38ba3d35d006255ee9a82987337dc681
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-05-07 18:29:59 +0200

    feat: osmia_npm_version constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 use wasm_bindgen::prelude::*;
 use osmia::Osmia;
 
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn run_osmia(mut osmia: Osmia, code: &str) -> Result<String, String> {
+	osmia.run_code(&format!("{{{{ _OSMIA_NPM_VERSION = \"{}\" }}}}", VERSION)).unwrap();
+	osmia.run_code(code)
+}
+
 #[wasm_bindgen]
-pub fn run(code: String) -> Result<String, String> {
-	Osmia::default().run_code(&code)
+pub fn run(code: &str) -> Result<String, String> {
+	run_osmia(Osmia::default(), &code)
 }
