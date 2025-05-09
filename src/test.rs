87335fca31574fc33da07b531c08ec5e384cbc1d
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-05-09 18:37:13 +0200

    feat: stable version + macro_tests

--- src/test.rs
+++ src/test.rs
@@ -1,44 +1,41 @@
 use osmia::{Osmia, CodeInterpreter};
 use crate::{run, run_ctx, VERSION};
+use macro_test::macro_tests;
 
 fn rok_eq(code: &str, expected: &str) {
 	println!("Running code (should be ok):\n{}\nexpected:\n{}", code, expected);
 	let r = run(code);
-	ok(&r);
+	assert!(r.is_ok());
 	assert_eq!(r.unwrap(), expected);
 }
 
 fn rctx_eq(json: &str, code: &str, expected: &str) {
 	println!("Running code (should be ok):\n{}\ncontext:\n{}\nexpected:\n{}", code, json, expected);
 	let r = run_ctx(json, code);
-	ok(&r);
+	assert!(r.is_ok());
 	assert_eq!(r.unwrap(), expected);
 }
 
-fn ok(result: &Result<String, String>) {
-	println!("{:?}", result);
-	assert!(result.is_ok());
+fn err(code: &str) {
+	println!("Running code (should fail):\n{}", code);
+	let r = run(code);
+	assert!(r.is_err());
 }
 
-fn err(result: &Result<String, String>) {
-	println!("{:?}", result);
-	assert!(result.is_err());
-}
+macro_tests!(
+	rok_eq,
+	(osmia_version, "{{ _OSMIA_VERSION }}", Osmia::VERSION),
+	(osmia_npm_version, "{{ _OSMIA_NPM_VERSION }}", VERSION),
+);
 
-#[test]
-fn osmia_version() {rok_eq("{{ _OSMIA_VERSION }}", Osmia::VERSION)}
+macro_tests!(
+	rctx_eq,
+	(json, r#"{"usr":{"name":"Marvin"}}"#, "{{usr.name}}", "Marvin")
+);
 
-#[test]
-fn osmia_npm_version() {rok_eq("{{ _OSMIA_NPM_VERSION }}", VERSION)}
-
-#[test]
-fn json() {rctx_eq(r#"{"usr":{"name":"Marvin"}}"#, "{{usr.name}}", "Marvin")}
-
-#[test]
-fn fail_ctx() {err(&run("{{usr.name}}"))}
-
-#[test]
-fn fail_code01() {err(&run("{{if }}"))}
-
-#[test]
-fn fail_code02() {err(&run("{{if true}}foo"))}
+macro_tests!(
+	err,
+	(fail_ctx, "{{usr.name}}"),
+	(fail_code01, "{{if }}"),
+	(fail_code02, "{{if true}}foo")
+);
