e6699c61c3f2ba45d3c1e62a1214e4aa7b5634c7
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-05-31 11:14:22 +0200

    refactor: npm_ctx + ctx2osmia

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,12 @@ mod test;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn run_osmia(mut osmia: Osmia, code: &str) -> Result<String, String> {
+fn add_npm_ctx(osmia: &mut Osmia) {
 	osmia.run_code(&format!("{{{{ _OSMIA_NPM_VERSION = \"{}\" }}}}", VERSION)).unwrap();
+}
+
+fn run_osmia(mut osmia: Osmia, code: &str) -> Result<String, String> {
+	add_npm_ctx(&mut osmia);
 	osmia.run_code(code)
 }
 
@@ -21,12 +25,16 @@ pub fn run_ctx(ctx: &str, code: &str) -> Result<String, String> {
 	run_osmia(Osmia::try_from(ctx)?, &code)
 }
 
-#[wasm_bindgen]
-pub fn ctx_json_dump(ctx: Option<String>) -> Result<String, String> {
+fn ctx2osmia(ctx: Option<String>) -> Result<Osmia, String> {
 	let mut osmia = match ctx {
 		Some(ctx) => Osmia::try_from(ctx.as_str())?,
 		None => Osmia::default(),
 	};
-	osmia.run_code(&format!("{{{{ _OSMIA_NPM_VERSION = \"{}\" }}}}", VERSION)).unwrap();
-	Ok(osmia.ctx_json_dump())
+	add_npm_ctx(&mut osmia);
+	Ok(osmia)
+}
+
+#[wasm_bindgen]
+pub fn ctx_json_dump(ctx: Option<String>) -> Result<String, String> {
+	Ok(ctx2osmia(ctx)?.ctx_json_dump())
 }
